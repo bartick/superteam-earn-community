81f6fb64349fee2a025c05fbc89aaf362fe18609
bartick <bartickmaiti1998@gmail.com>
2024-06-06 22:41:27 +0530

    change: set Database url as static

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -1,7 +1,18 @@
+use std::env;
+use once_cell::sync::Lazy;
 use diesel::{pg::PgConnection, r2d2::{Pool, ConnectionManager}};
 
+/**
+ * This is the database URL
+ */
+pub(crate) static DATABASE_URL: Lazy<String> = Lazy::new(|| {
+    env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in the environment")
+});
+
+/**
+ * This function returns a connection pool to the database
+ */
 pub fn get_connection_pool() -> Pool<ConnectionManager<PgConnection>> {
-    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<PgConnection>::new(url);
+    let manager = ConnectionManager::<PgConnection>::new(DATABASE_URL.to_string());
     Pool::builder().build(manager).expect("Failed to create pool")
 }
