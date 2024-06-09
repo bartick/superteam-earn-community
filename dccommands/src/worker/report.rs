b7e4b1eda78dd0ff7c4b172c2f3d6c88ad73cb53
bartick <bartickmaiti1998@gmail.com>
2024-06-09 21:16:13 +0530

    fix: properly handling rate limit

--- dccommands/src/worker/report.rs
+++ dccommands/src/worker/report.rs
@@ -1,11 +1,10 @@
 use diesel::{pg::PgConnection, query_dsl::QueryDsl, r2d2::{ConnectionManager, Pool}, ExpressionMethods, RunQueryDsl};
-use twilight_http::Client;
 use twilight_model::id::Id;
 use database::{models::{discord::DiscordSettings, posts::Post}, schema::discord_settings};
 
 use crate::utils::get::get_client;
 
-pub async fn report(pool: Pool<ConnectionManager<PgConnection>>, post: Post) {
+pub async fn report(pool: Pool<ConnectionManager<PgConnection>>, bounties: Vec<Post>, projects: Vec<Post>) {
     let servers = get_servers().await;
 
     let connection = &mut pool.get().expect("Failed to get connection");
@@ -14,9 +13,10 @@ pub async fn report(pool: Pool<ConnectionManager<PgConnection>>, post: Post) {
     let settings: Vec<DiscordSettings> = discord_settings::dsl::discord_settings.filter(discord_settings::id.eq_any(servers)).load::<DiscordSettings>(connection).expect("Failed to get DiscordSettings");
 
     for setting in settings {
-        let cloned_post = post.clone();
+        let cloned_bounties = bounties.clone();
+        let cloned_projects = projects.clone();
         tokio::spawn(async move {
-            send_report(setting, cloned_post).await;
+            send_report(setting, cloned_bounties, cloned_projects).await;
         });
     }
 }
@@ -39,23 +39,49 @@ async fn get_servers() -> Vec<i64> {
     guild_ids
 }
 
-async fn send_report(setting: DiscordSettings, post: Post){
-    let channel: u64;
-    if post._type == Some(String::from("bounty")) {
-        channel = setting.bounty_id.expect("Not found bounty channel") as u64;
-    } else if post._type == Some(String::from("project")) {
-        channel = setting.project_id.expect("Not found project channel") as u64;
+async fn send_report(setting: DiscordSettings, bounties: Vec<Post>, projects: Vec<Post>){
+    if setting.bounty_id == setting.project_id {
+        let posts: Vec<Post> = bounties.iter().chain(projects.iter()).cloned().collect();
+        let channel = setting.bounty_id.unwrap() as u64;
+        tokio::spawn(async move {
+            send_message(channel, posts).await;
+        });
     } else {
-        return;
+        let bounty_channel = setting.bounty_id.unwrap() as u64;
+        let project_channel = setting.project_id.unwrap() as u64;
+        tokio::spawn(async move {
+            send_message(bounty_channel, bounties).await;
+        });
+
+        tokio::spawn(async move {
+            send_message(project_channel, projects).await;
+        });
     }
-    let client = get_client();
-    send_message(&client, channel, format!("**{}**\nhttps://earn.superteam.fun/listings/{}/{}", post.title.unwrap_or(String::from("Title Not Found")), post._type.unwrap(),post.slug.unwrap_or(String::from("")))).await;
 }
 
-async fn send_message(client: &Client, channel_id: u64, message: String) {
-    client.create_message(Id::new(channel_id))
-        .content(message.as_str())
-        .expect(format!("Failed to send message to channel: {}", channel_id).as_str())
-        .await
-        .expect(format!("Failed to send message to channel: {}", channel_id).as_str());
+async fn send_message(channel_id: u64, posts: Vec<Post>) {
+    let mut _response_number = 0;
+
+    // send 5 tokio spawn response messages per second
+    for post_chunk in posts.chunks(5) {
+
+        let mut handles = Vec::new();
+
+        for post in post_chunk.to_vec() {
+            let handle = tokio::spawn(async move {
+                let client = get_client();
+                client.create_message(Id::new(channel_id))
+                    .content(format!("**{}**\nhttps://earn.superteam.fun/listings/{}/{}", post.title.unwrap_or(String::from("Title Not Found")), post._type.unwrap(), post.slug.unwrap_or(String::from(""))).as_str())
+                    .expect(format!("Failed to send message to channel: {}", channel_id).as_str())
+                    .await
+                    .expect(format!("Failed to send message to channel: {}", channel_id).as_str());
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.await.unwrap();
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
+    }
 }
