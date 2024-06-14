c8b69e327d912fa3ddd710bb7e98dd02e4305704
bartick <bartickmaiti1998@gmail.com>
2024-06-14 14:43:24 +0530

    add: string to enum converter

--- telebot/src/helpers/commands.rs
+++ telebot/src/helpers/commands.rs
@@ -1,8 +1,31 @@
 use teloxide::macros::BotCommands;
 
-#[derive(BotCommands, Clone)]
+#[derive(BotCommands, Clone, Debug)]
 #[command(rename_rule = "lowercase", parse_with = "split")]
-enum Command {
+pub enum Command {
+    Start,
+    Stop,
     Help,
-    Start
+}
+
+impl Command {
+    /**
+     * Parse command from string
+     * 
+     * @param s: &str
+     * 
+     * @return Option<Self>
+     * 
+     * @example Command::from_str("/start")
+     * 
+     * @description This function is used to parse a command from a string
+     */
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "/start" => Some(Self::Start),
+            "/stop" => Some(Self::Stop),
+            "/help" => Some(Self::Help),
+            _ => None
+        }
+    }
 }
