7eec9820d9e8224c5b5f305108d28368e23f3e36
bartick <bartickmaiti1998@gmail.com>
2024-06-17 01:14:34 +0530

    fix: handled topics

--- telebot/src/handlers/chat.rs
+++ telebot/src/handlers/chat.rs
@@ -1,6 +1,6 @@
-use teloxide::prelude::{Bot, Message};
+use teloxide::{prelude::Message, requests::Requester};
 
-use crate::{commands::{help::handle_help, _404::handle_404}, handlers::message::command_handler, helpers::commands::Command};
+use crate::{commands::{help::handle_help, _404::handle_404}, constants::BOT, handlers::message::command_handler, helpers::commands::Command};
 
 /**
  * Private chat handler function
@@ -14,7 +14,7 @@ use crate::{commands::{help::handle_help, _404::handle_404}, handlers::message::
  * 
  * @description This function is used to handle private chat messages from the user
  */
-pub async fn private_chat_handler(bot:Bot, message: Message) {
+pub async fn private_chat_handler(message: Message) {
     let cmd = command_handler(message.clone());
     if cmd.is_none() {
         return
@@ -32,95 +32,13 @@ pub async fn private_chat_handler(bot:Bot, message: Message) {
 
     match cmd.unwrap() {
         Command::Start => {
-            handle_404(bot, chat_id).await;
+            handle_404(chat_id, None).await;
         },
         Command::Stop => {
-            handle_404(bot, chat_id).await;
+            handle_404(chat_id, None).await;
         },
         Command::Help => {
-            handle_help(bot, chat_id).await;
-        }
-    }
-}
-
-/**
- * Public chat channel handler function
- * 
- * @param bot: Bot
- * @param message: Message
- * 
- * @return ()
- * 
- * @example public_chat_channel_handler(bot, message).await
- * 
- * @description This function is used to handle public chat messages from the channel
- */
-pub async fn public_chat_channel_handler(bot:Bot, message: Message) {
-    let cmd = command_handler(message.clone());
-    if cmd.is_none() {
-        return
-    }
-
-    let cmd = Command::from_str(&cmd.unwrap().split(|c| {
-        c == ' ' || c == '@'
-    }).collect::<Vec<&str>>()[0]);
-
-    if cmd.is_none() {
-        return
-    }
-
-    let chat_id = message.chat.id;
-
-    match cmd.unwrap() {
-        Command::Start => {
-            handle_404(bot, chat_id).await;
-        },
-        Command::Stop => {
-            handle_404(bot, chat_id).await;
-        },
-        Command::Help => {
-            handle_help(bot, chat_id).await;
-        }
-    }
-}
-
-/**
- * Public chat group handler function
- * 
- * @param bot: Bot
- * @param message: Message
- * 
- * @return ()
- * 
- * @example public_chat_group_handler(bot, message).await
- * 
- * @description This function is used to handle public chat messages from the group
- */
-pub async fn public_chat_group_handler(bot:Bot, message: Message) {
-    let cmd = command_handler(message.clone());
-    if cmd.is_none() {
-        return
-    }
-
-    let cmd = Command::from_str(&cmd.unwrap().split(|c| {
-        c == ' ' || c == '@'
-    }).collect::<Vec<&str>>()[0]);
-    
-    if cmd.is_none() {
-        return
-    }
-
-    let chat_id = message.chat.id;
-
-    match cmd.unwrap() {
-        Command::Start => {
-            handle_404(bot, chat_id).await;
-        },
-        Command::Stop => {
-            handle_404(bot, chat_id).await;
-        },
-        Command::Help => {
-            handle_help(bot, chat_id).await;
+            handle_help(chat_id, None).await;
         }
     }
 }
@@ -137,31 +55,46 @@ pub async fn public_chat_group_handler(bot:Bot, message: Message) {
  * 
  * @description This function is used to handle public chat messages from the supergroup
  */
-pub async fn public_chat_supergroup_handler(bot:Bot, message: Message) {
+pub async fn public_chat_supergroup_handler(message: Message) {
     let cmd = command_handler(message.clone());
     if cmd.is_none() {
         return
     }
 
-    let cmd = Command::from_str(&cmd.unwrap().split(|c| {
+    let cmd = cmd.unwrap();
+
+    let cmd: Vec<&str> = cmd.split(|c| {
         c == ' ' || c == '@'
-    }).collect::<Vec<&str>>()[0]);
+    }).collect::<Vec<&str>>();
+    
+    if cmd.len() <= 1 {
+        return;
+    }
+
+    let bot_user = BOT.get_me().await.unwrap().username.clone().unwrap();
+
+    if cmd[1].ne(bot_user.as_str()) {
+        return;
+    }
+
+    let cmd = Command::from_str(cmd[0]);
     
     if cmd.is_none() {
         return
     }
 
     let chat_id = message.chat.id;
+    let thread_id = message.thread_id;
 
     match cmd.unwrap() {
         Command::Start => {
-            handle_404(bot, chat_id).await;
+            handle_404(chat_id, thread_id).await;
         },
         Command::Stop => {
-            handle_404(bot, chat_id).await;
+            handle_404(chat_id, thread_id).await;
         },
         Command::Help => {
-            handle_help(bot, chat_id).await;
+            handle_help(chat_id, thread_id).await;
         }
     }
 }
