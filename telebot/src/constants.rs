912dd039c4f53ef72b15218cd9a1d92ed2e5ab16
bartick <bartickmaiti1998@gmail.com>
2024-06-17 01:15:25 +0530

    add: bot cache

--- telebot/src/constants.rs
+++ telebot/src/constants.rs
@@ -1,6 +1,12 @@
 use std::env;
 use once_cell::sync::Lazy;
+use teloxide::{adaptors::cache_me::CacheMe, prelude::Bot, requests::RequesterExt};
 
 pub(crate) static TOKEN: Lazy<String> = Lazy::new(|| {
     env::var("TELOXIDE_TOKEN").expect("Expected TELOXIDE_TOKEN in the environment")
+});
+
+pub(crate) static BOT: Lazy<CacheMe<Bot>> = Lazy::new(|| {
+    Bot::new(TOKEN.to_string())
+        .cache_me()
 });
